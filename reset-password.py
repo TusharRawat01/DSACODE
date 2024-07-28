23f4e9beca6dafb044111c229b6d40d3f6d44819
Tushar Rawat <memewala699@gmail.com>
2024-07-28 13:25:34 +0530

    added reset-password

---
+++ reset-password.py
@@ -0,0 +1,8 @@
+username = input("Enter username  = ")
+password = input("Enter pass  = ")
+
+
+if username == "Admin" and password == "Admin":
+    print("You can reset")
+else:
+    print("You cannot reset")
